3334a55d7a398ff9f79b50142ea0e258055e956f
Stefan Senk <stefan@senktec.com>
2025-06-12 14:28:44 +0100

    Fix to namespace errors from zeitwerk.

--- app/controllers/spree_volume_pricing/admin/variants_controller_decorator.rb
+++ app/controllers/spree_volume_pricing/admin/variants_controller_decorator.rb
@@ -1,38 +1,36 @@
 module SpreeVolumePricing
-  module Spree
-    module Admin
-      module VariantsControllerDecorator
-        def edit
-          @variant.volume_prices.build(store_id: current_store.id) if @variant.volume_prices.where(store_id: current_store.id)
-          super
-        end
+  module Admin
+    module VariantsControllerDecorator
+      def edit
+        @variant.volume_prices.build(store_id: current_store.id) if @variant.volume_prices.where(store_id: current_store.id)
+        super
+      end
 
-        def volume_prices
-          @product = @variant.product
-          @variant.volume_prices.build(store_id: current_store.id) if @variant.volume_prices.where(store_id: current_store.id)
-        end
+      def volume_prices
+        @product = @variant.product
+        @variant.volume_prices.build(store_id: current_store.id) if @variant.volume_prices.where(store_id: current_store.id)
+      end
 
-        private
+      private
 
-        # This loads the variant for the master variant volume price editing
-        def load_resource_instance
-          parent
-          if new_actions.include?(params[:action].to_sym)
-            build_resource
-          elsif params[:id]
-            ::Spree::Variant.find(params[:id])
-          end
+      # This loads the variant for the master variant volume price editing
+      def load_resource_instance
+        parent
+        if new_actions.include?(params[:action].to_sym)
+          build_resource
+        elsif params[:id]
+          ::Spree::Variant.find(params[:id])
         end
+      end
 
-        def location_after_save
-          if @product.master.id == @variant.id && params[:variant].key?(:volume_prices_attributes)
-            return volume_prices_admin_product_variant_url(@product, @variant)
-          end
-          super
+      def location_after_save
+        if @product.master.id == @variant.id && params[:variant].key?(:volume_prices_attributes)
+          return volume_prices_admin_product_variant_url(@product, @variant)
         end
+        super
       end
     end
   end
 end
 
-::Spree::Admin::VariantsController.prepend SpreeVolumePricing::Spree::Admin::VariantsControllerDecorator
+::Spree::Admin::VariantsController.prepend ::SpreeVolumePricing::Admin::VariantsControllerDecorator
